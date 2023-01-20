071bbfd6a471309b68c4a4527908b62e2a908db1
Pama Lee <pama@pamalee.cn>
2023-01-21 03:04:33 +0800

    First Update
    第一次上传 初始化项目

---
+++ src/main/java/cn/devspace/ribosome/Main.java
@@ -0,0 +1,10 @@
+package cn.devspace.ribosome;
+
+import cn.devspace.nucleus.Plugin.PluginBase;
+
+public class Main extends PluginBase {
+    @Override
+    public void onLoad() {
+        sendLog("Ribosome加载中...");
+    }
+}
